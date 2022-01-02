df25af2466d35ac7b2bd4ceaffd8bde1d8acbe5a
Nikesh Sitaula <31296289+nikeshsitaula@users.noreply.github.com>
2022-01-02 12:11:56 +1100

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends Controller
+{
+    public function register(request $request){
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email'=> 'required|string|unique:users,email',
+            'password'=> 'required|string|confirmed'
+        ]);
+        $user = User::create([
+           'name' =>$fields['name'],
+           'email'=>$fields['email'],
+           'password'=>bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response,201);
+    }
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message'=>'Logged Out'
+        ];
+    }
+
+    public function login(request $request){
+        $fields = $request->validate([
+            'email'=> 'required|string',
+            'password'=> 'required|string'
+        ]);
+        //check email
+        $user = User::where('email',$fields['email'])->first();
+
+        //check password
+        if(!$user || !Hash::check($fields['password'],$user->password)){
+            return response([
+               'message' => 'The username or the password is incorrect'
+            ],401);
+        }
+
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response,201);
+    }
+}
